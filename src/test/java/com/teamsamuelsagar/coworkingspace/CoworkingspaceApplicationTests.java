64916f2e0877aa939566d01c3850ed17f8ca2ed6
Donald Langston <donald_langston@yahoo.com>
2025-07-14 18:11:39 -0400

    Initial spring boot scaffolding

---
+++ src/test/java/com/teamsamuelsagar/coworkingspace/CoworkingspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsamuelsagar.coworkingspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
