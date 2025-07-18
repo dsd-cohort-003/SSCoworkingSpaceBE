7338aca19f649c28d5b5f1d5392d574cbfcd9663
Spencer <sjlillian8@gmail.com>
2025-07-18 14:38:33 -0500

    Added generic methods to the Abstract class, as well as more specific fields to the child classes.

--- src/main/java/com/teamsamuelsagar/coworkingspace/pojos/ResourceAbstract.java
+++ src/main/java/com/teamsamuelsagar/coworkingspace/pojos/ResourceAbstract.java
@@ -31,4 +31,94 @@ public abstract class ResourceAbstract {
     @Setter
     protected double price;
 
+    /**
+     * Checks if the resource is available.
+     * 
+     * @return true if the resource is available (i.e., the quantity is greater than zero), false otherwise.
+     */
+    public boolean isAvailable() {
+        return available > 0;
+    }
+
+    /**
+     * Returns a summary of the resource, useful for logging or displaying information.
+     * <p>
+     * The summary will be in the format of a JSON object, with the following fields:
+     * <ul>
+     *     <li>id: the unique identifier of the resource</li>
+     *     <li>TYPE: the type of the resource, e.g. "Display", "Audio", etc.</li>
+     *     <li>name: the name of the resource</li>
+     *     <li>description: a brief description of the resource</li>
+     *     <li>available: the number of resources available</li>
+     *     <li>price: the price of the resource in dollars</li>
+     * </ul>
+     * </p>
+     */
+    public String getSummary() {
+        return "Resource{\n" +
+                "id = " + id +
+                ", TYPE = '" + TYPE + '\'' +
+                ", name = '" + name + '\'' +
+                ", description = '" + description + '\'' +
+                ", available = " + available +
+                ", price = $" + price +
+                "\n}";
+    }
+
+    /**
+     * Compares this resource with another resource to determine equality.
+     *
+     * @param resource the resource to compare with this resource
+     * @return true if the IDs of both resources are equal, false otherwise
+     */
+    public boolean equals(ResourceAbstract resource) {
+        return this.id == resource.getId();
+    }
+
+    /**
+     * Attempts to reserve one resource, if available.
+     * <p>
+     * If there is at least one resource available, this method will decrement the available count by one and return true.
+     * Otherwise, it will return false.
+     * </p>
+     * @return true if the resource was reserved, false if not
+     */
+    public boolean reserveOne() {
+        if (available > 0) {
+            available--;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Attempts to reserve multiple resources, if available.
+     * <p>
+     * If there are at least <code>quantity</code> resources available, this method will decrement the available count by <code>quantity</code> and return true.
+     * Otherwise, it will return false.
+     * </p>
+     * @param quantity the number of resources to reserve
+     * @return true if the resources were reserved, false if not
+     */
+    public boolean reserve(int quantity) {
+        if (available >= quantity) {
+            available -= quantity;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Attempts to return one resource, if not already fully reserved.
+     * <p>
+     * If there are available resources, this method will increment the available count by one and return true.
+     * Otherwise, it will return false.
+     * </p>
+     * @return true if the resource was returned, false if not
+     */
+    public boolean returnOne() {
+        available++;
+        return true;
+    }
+
 }
--- src/main/java/com/teamsamuelsagar/coworkingspace/pojos/ResourceVideo.java
+++ src/main/java/com/teamsamuelsagar/coworkingspace/pojos/ResourceVideo.java
@@ -1,8 +1,23 @@
 package com.teamsamuelsagar.coworkingspace.pojos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 //import com.teamsamuelsagar.coworkingspace.pojos.ResourceType;
 
 public class ResourceVideo extends ResourceAbstract {
+
+    @Getter
+    @Setter
+    private String fieldOfView;
+
+    @Getter
+    @Setter
+    private boolean isPortable;
+
+    @Getter
+    @Setter
+    private boolean hasMicrophone;
 
     public ResourceVideo() {
         this.TYPE = "Video"; // ResourceType.Video;
@@ -15,6 +30,33 @@ public class ResourceVideo extends ResourceAbstract {
         this.price = price;
         this.name = name;
         this.description = description;
+    }
+
+    public ResourceVideo(long id, int available, double price, String name, String description, String fov, boolean portable, boolean hasMicrophone) {
+        this.TYPE = "Video"; // ResourceType.Video;
+        this.id = id;
+        this.available = available;
+        this.price = price;
+        this.name = name;
+        this.description = description;
+        this.fieldOfView = fov;
+        this.isPortable = portable;
+        this.hasMicrophone = hasMicrophone;
+    }
+
+    @Override
+    public String getSummary() {
+        return "Resource{\n" +
+                "id = " + id +
+                ", TYPE = '" + TYPE + '\'' +
+                ", name = '" + name + '\'' +
+                ", description = '" + description + '\'' +
+                ", available = " + available +
+                ", price = $" + price +
+                ", fieldOfView = '" + fieldOfView + '\'' +
+                ", isPortable = " + isPortable +
+                ", hasMicrophone = " + hasMicrophone +
+                "\n}";
     }
 
 }
