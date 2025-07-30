63eaab5b035f7a6dd37fa71b194968260879fab2
Msheph1 <max.r.shep@gmail.com>
2025-07-30 17:42:46 -0500

    updated office entity to match erd and added office controller with delete, create, retrieve offices

---
+++ src/main/java/com/teamsamuelsagar/coworkingspace/service/OfficeService.java
@@ -0,0 +1,37 @@
+package com.teamsamuelsagar.coworkingspace.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.teamsamuelsagar.coworkingspace.model.Office;
+import com.teamsamuelsagar.coworkingspace.repository.OfficeRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class OfficeService {
+    private final OfficeRepository officeRepository;
+
+
+    public List<Office> getAllOffices() {
+        return officeRepository.findAll();
+    }
+
+    public Office createOffice(Office office) {
+        return officeRepository.save(office);
+    }
+
+    public void deleteOffice(long officeId) {
+        if(officeRepository.existsById(officeId)) {
+            officeRepository.deleteById(officeId);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+    
+}
