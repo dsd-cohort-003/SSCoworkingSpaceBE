7bce6c9039cd320570b0324a3c94d26d773864da
Msheph1 <max.r.shep@gmail.com>
2025-07-31 21:39:22 -0500

    added query to handle availabiltiy check for specific office id

--- src/main/java/com/teamsamuelsagar/coworkingspace/repository/DeskRepository.java
+++ src/main/java/com/teamsamuelsagar/coworkingspace/repository/DeskRepository.java
@@ -1,6 +1,6 @@
 package com.teamsamuelsagar.coworkingspace.repository;
 
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,8 +15,10 @@ public interface DeskRepository extends JpaRepository<Desk, Long> {
     List<Desk> findByOfficeId(Long officeId);
 
     @Query("""
-            SELECT d FROM Desk d WHERE d.office.id = :officeId
+            SELECT d FROM Desk d WHERE d.office.id = :officeId and d.id NOT IN 
+            (SELECT dr.desk.id FROM DeskReservation dr where dr.startDate <= :endDate AND dr.endDate >= :startDate)
             """)
-    List<Desk> findAvailableDesks(@Param("officeId") Long officeId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    List<Desk> findAvailableDesks(@Param("officeId") Long officeId,
+                                  @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
     
 }
