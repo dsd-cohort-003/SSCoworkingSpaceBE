5d9e1eeb90d19f65949e6c0f793dd77c67c8ad47
Spencer <sjlillian8@gmail.com>
2025-08-06 08:33:08 -0500

    Fixed duplicate properties as well as included comments and other nuances in the code. The logic has not been changed!

--- src/main/java/com/teamsamuelsagar/coworkingspace/model/ResourceReservation.java
+++ src/main/java/com/teamsamuelsagar/coworkingspace/model/ResourceReservation.java
@@ -4,7 +4,14 @@ import java.time.LocalDate;
 
 import com.teamsamuelsagar.coworkingspace.model.enumtype.ReservationStatus;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/teamsamuelsagar/coworkingspace/service/ResourceReservationService.java
+++ src/main/java/com/teamsamuelsagar/coworkingspace/service/ResourceReservationService.java
@@ -16,8 +16,19 @@ public class ResourceReservationService {
     @Autowired
     private ResourceReservationRepository resourceReservationRepository;
 
+    /**
+     * Retrieves a list of active reservations for the given resource id and date range.
+     * <p>
+     * A reservation is considered active if it is not complete and the date range overlaps
+     * with the reservation's start and end dates.
+     * </p>
+     * @param resourceId the id of the resource to search by
+     * @param startDate the start date of the range to search
+     * @param endDate the end date of the range to search
+     * @return a list of active reservations
+     */
     public List<ResourceReservation> getReservationByResourceId(long resourceId, LocalDate startDate, LocalDate endDate) {
         return resourceReservationRepository.findActiveReservations(resourceId, startDate, endDate, List.of(ReservationStatus.PENDING, ReservationStatus.APPROVED));
-    }
+    } // The ReservationStatus enums can easily be swapped out
 
 }
